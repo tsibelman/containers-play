bd16b3e897c812f8ee7e42742a316113fcc0aaa7
Michael Tsibelman <michaelt@docontrol.io>
2024-04-05 22:02:59 +0300

    Add app image to docker compose

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -68,12 +68,7 @@ export default function Index() {
 
         <div className="mx-auto max-w-7xl px-4 py-2 sm:px-6 lg:px-8">
           <div className="mt-6 flex flex-wrap justify-center gap-8">
-            {[
-              {
-                src: "https://user-images.githubusercontent.com/1500684/157764397-ccd8ea10-b8aa-4772-a99b-35de937319e1.svg",
-                alt: "Fly.io",
-                href: "https://fly.io",
-              },
+            {[       
               {
                 src: "https://user-images.githubusercontent.com/1500684/158238105-e7279a0c-1640-40db-86b0-3d3a10aab824.svg",
                 alt: "PostgreSQL",
